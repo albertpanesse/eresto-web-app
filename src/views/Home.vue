4678e0523f219ef9dd384a5cb73b4d8de39ec3c9
Albert Panesse <albert.panesse@gmail.com>
2020-04-11 06:30:44 +0700

    node ver

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home-wrapper">
     <div class="btn-table" v-for="n in 80" v-bind:key="n">
-      <button class="btn btn-light">{{ n }}</button>
+      <button class="btn btn-secondary">{{ n }}</button>
     </div>
   </div>
 </template>
@@ -17,9 +17,14 @@ export default {
   display: flex;
   flex-direction: row;
   flex-wrap: wrap;
+  padding: 10px;
 }
 .home-wrapper .btn-table {
-  width: 6%;
-  margin-bottom: 20px;
+  width: 10%;
+  padding: 0 10px 10px 10px;
+  margin-bottom: 10px;
+}
+.home-wrapper .btn-table button {
+  width: 100%;
 }
 </style>
